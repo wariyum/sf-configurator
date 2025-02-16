3b5a6091ca0009d0f8cd9c18e2d0ecf02be70fe3
Mohammed <nazeel@wariyum.com>
2025-02-16 12:13:12 +0530

    aligned left and right

--- src/app/homepage-configurator/homepage-configurator.component.ts
+++ src/app/homepage-configurator/homepage-configurator.component.ts
@@ -2,11 +2,13 @@ import { Component, signal } from '@angular/core';
 import { CdkDragDrop, CdkDropList, CdkDrag, moveItemInArray } from '@angular/cdk/drag-drop';
 import { FormsModule } from '@angular/forms';
 import { HompageConfiguratorService } from './hompage-configurator.service';
+import { Section } from './homepageConfigurator.types';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-homepage-configurator',
   standalone: true,
-  imports: [CdkDropList, CdkDrag, FormsModule],
+  imports: [CdkDropList, CdkDrag, FormsModule,CommonModule],
   templateUrl: './homepage-configurator.component.html',
   styleUrl: './homepage-configurator.component.css'
 })
@@ -14,19 +16,20 @@ export class HomepageConfiguratorComponent {
 
   constructor(private homepageConfigService: HompageConfiguratorService) {
     this.nearmeItems = this.homepageConfigService.sections;
-
   }
   nearmeItems: any[];
   selectedOption = signal<string>('Brand');
   isDragging = false;
+  selectedSection: Section| undefined;
 
   drop(event: CdkDragDrop<string[]>) {
     moveItemInArray(this.nearmeItems, event.previousIndex, event.currentIndex);
   }
 
-  showAlert(item: string) {
+  showAlert(item: Section) {
     if (!this.isDragging) {
-      alert(`You clicked: ${item}`);
+     //selectedSection should from the item
+     this.selectedSection = item;
     }
   }
 
