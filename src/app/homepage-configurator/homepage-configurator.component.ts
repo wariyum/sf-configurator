f08ae3c80cf97686d730b1044b922904cf38bf21
Mohammed <nazeel@wariyum.com>
2025-02-16 07:39:32 +0530

    fixed ngModel Issue

--- src/app/homepage-configurator/homepage-configurator.component.ts
+++ src/app/homepage-configurator/homepage-configurator.component.ts
@@ -1,33 +1,25 @@
-import {Component} from '@angular/core';
+import {Component, signal} from '@angular/core';
 import {CdkDragDrop, CdkDropList, CdkDrag, moveItemInArray} from '@angular/cdk/drag-drop';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-homepage-configurator',
   standalone: true,
-  imports: [CdkDropList, CdkDrag],
+  imports: [CdkDropList, CdkDrag, FormsModule],
   templateUrl: './homepage-configurator.component.html',
   styleUrl: './homepage-configurator.component.css'
 })
 export class HomepageConfiguratorComponent {
 
-
-  movies = [
-    'Episode I - The Phantom Menace',
-    'Episode II - Attack of the Clones',
-    'Episode III - Revenge of the Sith',
-    'Episode IV - A New Hope',
-    'Episode V - The Empire Strikes Back',
-    'Episode VI - Return of the Jedi',
-    'Episode VII - The Force Awakens',
-    'Episode VIII - The Last Jedi',
-    'Episode IX – The Rise of Skywalker',
+  selectedOption = signal<string>('Brand');
+  nearmeItems = [
+   'Banner1',"Category1"
   ];
 
- selectedOption: string = "Brand";
 
 
   drop(event: CdkDragDrop<string[]>) {
-    moveItemInArray(this.movies, event.previousIndex, event.currentIndex);
+    moveItemInArray(this.nearmeItems, event.previousIndex, event.currentIndex);
   }
 
 }
